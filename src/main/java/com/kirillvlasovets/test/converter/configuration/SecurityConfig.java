51f06f76dddb1d1c07e4e35602739a035aaf0ce2
Vlasovets Kirill <vlasovets.kirill@otr.ru>
2022-03-02 03:22:23 +0300

    Added roles permissions. Updated styles for part of HTML pages.

--- src/main/java/com/kirillvlasovets/test/converter/configuration/SecurityConfig.java
+++ src/main/java/com/kirillvlasovets/test/converter/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.kirillvlasovets.test.converter.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,14 +15,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private DataSource dataSource;
 
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/css/**", "/js/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/new_converting").hasAnyRole("BANKER")
+                .antMatchers("/converting_result").hasAnyRole("BANKER")
+                .antMatchers("/history").hasAnyRole("BANKER");
+
         http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/new_converting", true)
+                .defaultSuccessUrl("/", true)
                 .permitAll();
 
         http.logout()
